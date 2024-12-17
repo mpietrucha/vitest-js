3ca51bb440d3845111f5bf6418f81c5e88117b86
Michał Pietrucha <mpietrucha50@gmail.com>
2024-12-17 12:10:39 +0100

    wip

--- src/composables/useVitest.js
+++ src/composables/useVitest.js
@@ -1,4 +1,7 @@
 import { describe, expect, it, test } from 'vitest'
+import { defineConfig } from 'vitest/config'
+
+export const useConfig = defineConfig
 
 export const useIt = (fixtures = {}) => {
     return it.extend(fixtures)
@@ -19,6 +22,7 @@ export const useDescribe = () => {
 export default () => ({
     useIt,
     useTest,
+    useConfig,
     useExpect,
     useDescribe,
 })
